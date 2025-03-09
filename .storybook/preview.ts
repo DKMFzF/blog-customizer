ac4240bb6e668e80e9d205c35cbdf005c9f82c70
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-03-09 22:48:20 +0700

    fix(fix bug in storybook add support on build): downgrade version nodejs 23.09.0 > 18.xx.xx

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,17 +1,25 @@
-import type { Preview } from "@storybook/react";
-import { StoryDecorator } from "../src/ui/story-decorator/StoryDecorator";
+import type { Preview } from '@storybook/react';
+import { StoryDecorator } from '../src/ui/story-decorator/StoryDecorator';
+
+/**
+ * настройка препространство для storybook
+ * он настраивает поведение storybook для всех историй включая параметры и декараторы
+ */
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-  decorators: [StoryDecorator],
+	parameters: {
+		// глобальные параметры storybook
+		actions: { argTypesRegex: '^on[A-Z].*' }, // автоматическое отслеживание событий
+
+		// автоматичиское создание контроллеров для отслеживания событий
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+	decorators: [StoryDecorator],
 };
 
 export default preview;
