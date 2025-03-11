cb532a0f32e1febf1a1a687b7222fd48a9bf427f
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-03-12 01:48:28 +0700

    feat(select option): add option cascade on select

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,17 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
+import { SelectOption } from './SelectOption';
 import clsx from 'clsx';
-
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
+import { fontFamilyOptions, fontColors } from 'src/constants/articleProps';
 
 /**
- * боковая панель формы открытия настроек страницы
+ * Боковая панель формы открытия настроек страницы
  */
-
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
-
 	const handleOpenBtnClick = () => setIsOpen((isOpen) => !isOpen);
 
 	return (
@@ -20,6 +20,11 @@ export const ArticleParamsForm = () => {
 			<aside
 				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={clsx(styles.form)}>
+					<Text as='h2' size={31} weight={800} uppercase dynamicLite>
+						Задайте параметры
+					</Text>
+					<SelectOption title='Шрифт текста' options={fontFamilyOptions} />
+					<SelectOption title='Цвет фона' options={fontColors} />
 					<div className={clsx(styles.bottomContainer)}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
---
+++ src/components/article-params-form/SelectOption.tsx
@@ -0,0 +1,28 @@
+import { Select } from 'src/ui/select';
+import { useState } from 'react';
+import { OptionType } from 'src/constants/articleProps';
+
+type SelectOptionProps = {
+	title: string;
+	options: OptionType[];
+	onChange?: (selected: OptionType) => void;
+};
+
+export const SelectOption = (props: SelectOptionProps) => {
+	const { title, options, onChange } = props;
+	const [selected, setSelected] = useState<OptionType>(options[0]);
+
+	const handleChange = (option: OptionType) => {
+		setSelected(option);
+		onChange?.(option);
+	};
+
+	return (
+		<Select
+			selected={selected}
+			onChange={handleChange}
+			options={options}
+			title={title}
+		/>
+	);
+};
