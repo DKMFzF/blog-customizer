e085f97379b5d889d66a678a939a5b15f6716193
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-03-12 18:59:34 +0700

    fix(article params form): fix bug select option

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,18 +1,33 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { Text } from 'src/ui/text';
-import { SelectOption } from './SelectOption';
 import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
-import { fontFamilyOptions, fontColors } from 'src/constants/articleProps';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	// fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+	OptionType,
+} from 'src/constants/articleProps';
+import { Select } from 'src/ui/select';
 
 /**
  * Боковая панель формы открытия настроек страницы
  */
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	const handleOpenBtnClick = () => setIsOpen((isOpen) => !isOpen);
+
+	const handleChange = (key: keyof ArticleStateType, value: OptionType) =>
+		setArticleState((prev) => ({ ...prev, [key]: value }));
 
 	return (
 		<>
@@ -23,10 +38,42 @@ export const ArticleParamsForm = () => {
 					<Text as='h2' size={31} weight={800} uppercase dynamicLite>
 						Задайте параметры
 					</Text>
-					<SelectOption title='Шрифт текста' options={fontFamilyOptions} />
-					<SelectOption title='Цвет фона' options={fontColors} />
+
+					<Select
+						selected={articleState.fontFamilyOption}
+						onChange={(value) => handleChange('fontFamilyOption', value)}
+						options={fontFamilyOptions}
+						title='Шрифт'
+					/>
+
+					<Select
+						selected={articleState.fontColor}
+						onChange={(value) => handleChange('fontColor', value)}
+						options={fontColors}
+						title='Цвет шрифта'
+					/>
+
+					<Select
+						selected={articleState.backgroundColor}
+						onChange={(value) => handleChange('backgroundColor', value)}
+						options={backgroundColors}
+						title='Цвет фона'
+					/>
+
+					<Select
+						selected={articleState.contentWidth}
+						onChange={(value) => handleChange('contentWidth', value)}
+						options={contentWidthArr}
+						title='Ширина контента'
+					/>
+
 					<div className={clsx(styles.bottomContainer)}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={() => setArticleState(defaultArticleState)}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
